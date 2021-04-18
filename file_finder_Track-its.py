e94a1d4109ad7df67913c3b1149a891ceb2a289b
Erik Braun <30779061+erik98671@users.noreply.github.com>
2021-04-18 08:45:34 -0700

    Add files via upload

--- file_finder_Track-its.py
+++ file_finder_Track-its.py
@@ -5,25 +5,14 @@ import os
 import time
 import datetime
 
-root = r"H:\Timeline"
-output_filename = "file_finder_H_Track-it.txt"
-file_extension = ".docx"
+import File_Finder
 
-def main(root, output_filename,file_extension):
-    # Create output file
-    output_file = open(output_filename, "w")
-    print("Opened file", output_filename)
-    # Loop through the folders and files
-    for root, dirs, files in os.walk(root):
-        print("Starting at root",root)
-        for file in files:
-            #print("Exaluating file",file)
-            if file.endswith(file_extension) and file[0:8] == "Track-it":
-                file_time = time.ctime(os.path.getmtime(os.path.join(root, file)))
-                file_date = str(datetime.datetime.strptime(file_time, "%a %b %d %H:%M:%S %Y"))
-                output_file.write(str(file_date[0:10])+" "+os.path.join(root,file)+"\n")
-    return
+if __name__ == "__main__":
+    root = r"H:\Timeline"
+    output_filename = "file_finder_H_Track-it.txt"
+    file_extension = ".docx"
+    filename_prefix = "Track-it"
+    
+    finder = File_Finder.File_Finder(root, output_filename, file_extension, filename_prefix)
+    finder.main()
 
-print("The process is starting.")
-main(root,output_filename,file_extension)
-input("The process is complete. Press enter to quit.")
--- file_finder_ump.py
+++ file_finder_ump.py
@@ -8,9 +8,9 @@ import datetime
 import File_Finder
 
 if __name__ == "__main__":
-    root = r"H:\Timeline"
-    output_filename = "file_finder_H_Python.txt"
-    file_extension = ".py"
+    root = r"H:\Timeline\2020"
+    output_filename = "file_finder_H_2020\wdoc.txt"
+    file_extension = ""
     filename_prefix = ""
     
     finder = File_Finder(root, output_filename, file_extension, filename_prefix)
